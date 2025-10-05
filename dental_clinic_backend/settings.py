748d303970875117a4616de96d6f4b78ccae53c7
Daniel F.Banegas <danielfbanegas@gmail.com>
2025-10-05 07:06:21 -0400

    SI2-21 solucionss

--- dental_clinic_backend/settings.py
+++ dental_clinic_backend/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     ".amazonaws.com",
     "ec2-18-220-214-178.us-east-2.compute.amazonaws.com",
     "sitwo-project.onrender.com",
-    "http://127.0.0.1:5173"
+    "127.0.0.1:5173"
 ]
 
 CORS_ALLOWED_ORIGINS = [
